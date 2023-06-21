a5c5ac699d51f280158bb0fb617d5ae00b02dd02
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-06-21 14:06:19 +0500

    Personal_Portfolio

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="sticky top-0 z-50 bg-teal-100 border-2 border-b-gray-400">
+        <div class="container mx-auto h-[70px]">
+            <div class="flex jusrify-beetwen items-center h-[70px]">
+                <div class="w-1/2 flex items-center justify-beetwen ">
+                    <h1 class="text-[50px] font-semibold ml-5 text-yellow-400 ">
+                        Port<span class="text-teal-900 font-semibold">folio</span>
+                    </h1>
+                </div>
+                <div class="w-1/2 flex justify-end items-center font-bold gap-6 mr-5">
+                    <a href="#aboute" class="hover:text-red-500">Home</a>
+                    <a href="#aboute" class="hover:text-red-500">Aboute</a>
+                    <a href="#skills" class="hover:text-red-500">Skill</a>
+                    <a href="#project" class="hover:text-red-500">Project</a>
+                    <a href="/blog" class="hover:text-red-500">Blog</a>
+                    <a href="/workplace" class="hover:text-red-500">Workplace</a>
+                    <a href="#contacts" class="hover:text-red-500">Contact</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+    // import { onMounted } from 'vue';
+    // onMounted(() =>{
+    //     const contactsSection = document.getElementById('contacts')
+    // })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,117 @@
+<template>
+    <Header/>
+    <section id="aboute" class="bg-teal-100 py-[100px]">
+        <div class="container mx-auto py-10">
+            <div class="flex jusrify-beetwen items-center ">
+                <div class="w-1/2 grid items-center justify-center pl-[100px]">
+                    <div>
+                        <p class="font-bold text-[24px]">Hi Everyone, I am</p>
+                    </div>
+                    <div>
+                        <h1 class="font-bold text-[48px]">Usmanov Elyor Elmuratovich</h1>
+                    </div>
+                    <div>
+                        <p class="text-[20px]">I am a Full-stack (NodeJs+VueJs) developer</p>
+                    </div>
+                    <div class="flex justify-beetwen mt-10 items-center gap-10">
+                        <a href="../assets/Rezyume.pdf" download class="py-4 px-8 text-xl bg-blue-400 text-white font-bold rounded-xl hover:bg-blue-600">Download CV</a>
+                    </div>
+                </div>
+                <div class="w-1/2 flex justify-center items-center font-bold">
+                    <img src="../public/cropped-IMG_9768.JPG" alt="" class="rounded-full w-[400px] object-cover">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="py-10">
+        <div class="container mx-auto">
+            <div class="flex jusrify-beetwen items-center ">
+                <div class="w-1/2 grid items-center justify-beetwen">
+                    <img src="../public/aboute.png" alt="" class="w-[500px] ">
+                </div>
+                <div class="w-1/2 grid justify-beetwen items-center gap-6">
+                    <div>
+                        <p class="font-bold text-[24px] text-blue-500 font-bold">About</p>
+                    </div>
+                    <div>
+                        <h1 class="font-bold text-[48px] font-bold">About me</h1>
+                    </div>
+                    <div>
+                        <p class="text-[20px] text-justify">I am, Usmanov Elyor Elmuratovich, was born in 1987 in a family of workers. I am married, I have 4 children. I graduated from high school in 1994-2002. In 2002-2005, I graduated from Uchkurgan College of Economics, majoring in "Information Technologies". From 2005 to 2022, I had my own "Radio electronics and computer repair" workshop and worked there. In 2022-2023, I studied in the "Foundation" and "Bootcamp Full-stack (NodeJS+VueJs)" courses at the "Salvation Education" educational center.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="skills" class="bg-teal-100 py-10">
+        <div class="container mx-auto ">
+            <div class="grid jusrify-beetwen items-center ">
+                <div class=" grid items-center justify-center">
+                    <p class="font-bold text-[24px] text-blue-500 font-bold text-center">Skills</p>
+                    <h1 class="font-bold text-[48px] font-bold text-center">What I am capable of?</h1>
+                    <div class="flex justify-center items-center gap-10 mt-10">
+                        <img src="https://static.vecteezy.com/system/resources/previews/012/697/299/original/stylized-3d-html-logo-design-free-png.png" alt="HTML" class="w-[150px]">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="CSS" class="w-[150px]">
+                        <img src="https://www.pngitem.com/pimgs/m/27-278312_bootstrap-bootstrap-logo-black-and-white-hd-png.png" alt="Bootstrap" class="w-[150px]">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png" alt="Tailwind" class="w-[150px]">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/640px-Vue.js_Logo_2.svg.png" alt="Tailwind" class="w-[150px]">
+                        <img src="https://static-00.iconduck.com/assets.00/node-js-icon-1901x2048-mk1e13df.png" alt="Tailwind" class="w-[150px]">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="project" class="py-10">
+        <div class="container mx-auto ">
+            <div class="grid jusrify-beetwen items-center ">
+                <p class="font-bold text-[24px] text-blue-500 font-bold text-center">Projects</p>
+                <h1 class="font-bold text-[48px] font-bold text-center">What I have made?</h1>
+                    <div class="flex flex-wrap justify-center items-center gap-10 mt-10">
+                       <div class="w-[30%] h-full border card grid justify-beetwen shadow-lg shadow-gray-600 rounded-lg">
+                            <img src="../public/My_portfolio.png" alt="" class="w-full rounded-lg">
+                            <div class="px-5 py-5 grid gap-3 border-4 border-t-gray-500 rounded-lg ">
+                                <h1 class="font-bold text-[24px]">Portfolio Website Design</h1>
+                                <p class="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ipsa!</p>
+                                <div class="w-full text-blue-600 flex items-center justify-beetwen gap-6">
+                                    <a href="#">Project Link</a>
+                                    <a href="#">Github Link</a>
+                                </div>
+                            </div>
+                       </div>
+                       <!-- ==================================== -->
+                       <div class="w-[30%] h-full border card grid justify-beetwen shadow-lg shadow-gray-600 rounded-lg">
+                        <img src="../public/My_portfolio.png" alt="" class="w-full rounded-lg">
+                        <div class="px-5 py-5 grid gap-3 border-4 border-t-gray-500 rounded-lg ">
+                            <h1 class="font-bold text-[24px]">Portfolio Website Design</h1>
+                            <p class="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ipsa!</p>
+                            <div class="w-full text-blue-600 flex items-center justify-beetwen gap-6">
+                                <a href="#">Project Link</a>
+                                <a href="#">Github Link</a>
+                            </div>
+                        </div>
+                   </div>
+                   <!-- ============================================== -->
+                   <div class="w-[30%] h-full border card grid justify-beetwen shadow-lg shadow-gray-600 rounded-lg">
+                    <img src="../public/My_portfolio.png" alt="" class="w-full rounded-lg">
+                    <div class="px-5 py-5 grid gap-3 border-4 border-t-gray-500 rounded-lg ">
+                        <h1 class="font-bold text-[24px]">Portfolio Website Design</h1>
+                        <p class="text-[18px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, ipsa!</p>
+                        <div class="w-full text-blue-600 flex items-center justify-beetwen gap-6">
+                            <a href="#">Project Link</a>
+                            <a href="#">Github Link</a>
+                        </div>
+                    </div>
+               </div>
+               <!-- ================================ -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <Footer/>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
