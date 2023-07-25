552162922dd11d3c3a39bcda28be8c010cf6518e
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-07-25 17:39:54 +0500

    portfolio update

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,13 @@
 <template>
     <header class="sticky top-0 z-50 bg-teal-100 border-2 border-b-gray-400">
         <div class="container mx-auto h-[70px]">
-            <div class="flex jusrify-beetwen items-center h-[70px]">
+            <div class="flex justify-beetwen items-center h-[70px]">
                 <div class="w-1/2 flex items-center justify-beetwen ">
-                    <h1 class="text-[50px] font-semibold ml-5 text-yellow-400 ">
+                    <h1 class="portfolioo text-[50px] font-semibold ml-5 text-yellow-400 ">
                         Port<span class="text-teal-900 font-semibold">folio</span>
                     </h1>
                 </div>
-                <div class="w-1/2 flex justify-end items-center font-bold gap-6 mr-5">
+                <div class="menu w-1/2 flex justify-end items-center font-bold gap-6 mr-5">
                     <a href="#aboute" class="hover:text-red-500">Home</a>
                     <a href="#aboute" class="hover:text-red-500">Aboute</a>
                     <a href="#skills" class="hover:text-red-500">Skill</a>
@@ -22,12 +22,29 @@
 </template>
 
 <script setup>
-    // import { onMounted } from 'vue';
-    // onMounted(() =>{
-    //     const contactsSection = document.getElementById('contacts')
-    // })
 </script>
 
 <style lang="scss" scoped>
-
+    @media only screen and (max-width: 767px) {
+        .portfolioo{
+            display: none;
+        }
+        .menu{
+            width: 100%;
+            justify-content: center;
+        }
+    }
+   
+    @media only screen and (max-width: 548px) {
+        .portfolioo{
+            display: none;
+        }
+        .menu{
+            width: 100%;
+            justify-content: center;
+            font-size: 11px;
+            grid-gap: 15px;
+            padding-left: 10px;
+        }
+    }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
     <Header/>
     <section id="aboute" class="bg-teal-100 py-[100px]">
         <div class="container mx-auto py-10">
-            <div class="flex jusrify-beetwen items-center ">
-                <div class="w-1/2 grid items-center justify-center pl-[100px]">
+            <div class="hi flex jusrify-beetwen items-center ">
+                <div class="about w-1/2 grid items-center justify-center pl-[100px]">
                     <div>
                         <p class="font-bold text-[24px]">Hi Everyone, I am</p>
                     </div>
@@ -17,26 +17,26 @@
                         <a href="../public/Elyor Usmanov Resume.pdf" download class="py-4 px-8 text-xl bg-blue-400 text-white font-bold rounded-xl hover:bg-blue-600">Download CV</a>
                     </div>
                 </div>
-                <div class="w-1/2 flex justify-center items-center font-bold">
-                    <img src="../public/myphoto.png" alt="" class="rounded-full w-[400px] object-cover">
+                <div class="myypic w-1/2 flex justify-center items-center font-bold">
+                    <img src="../public/myphoto.png" alt="" class="mypic rounded-full w-[400px] object-cover">
                 </div>
             </div>
         </div>
     </section>
     <section class="py-10">
         <div class="container mx-auto">
-            <div class="flex jusrify-beetwen items-center ">
-                <div class="w-1/2 grid items-center justify-beetwen">
+            <div class="abc flex jusrify-beetwen items-center ">
+                <div class="abpic w-1/2 grid items-center justify-beetwen">
                     <img src="../public/aboute.png" alt="" class="w-[500px] ">
                 </div>
-                <div class="w-1/2 grid justify-beetwen items-center gap-6">
-                    <div>
+                <div class="about1 w-1/2 grid justify-beetwen items-center gap-6">
+                    <div class="haq">
                         <p class="font-bold text-[24px] text-blue-500 font-bold">About</p>
                     </div>
                     <div>
                         <h1 class="font-bold text-[48px] font-bold">About me</h1>
                     </div>
-                    <div>
+                    <div class="text">
                         <p class="text-[20px] text-justify">I am, Usmanov Elyor Elmuratovich, was born in 1987 in a family of workers. I am married, I have 4 children. I graduated from high school in 1994-2002. In 2002-2005, I graduated from Uchkurgan College of Economics, majoring in "Information Technologies". From 2005 to 2022, I had my own "Radio electronics and computer repair" workshop and worked there. In 2022-2023, I studied in the "Foundation" and "Bootcamp Full-stack (NodeJS+VueJs)" courses at the "Najot Ta'lim" educational center.</p>
                     </div>
                 </div>
@@ -63,10 +63,10 @@
             <div class="grid jusrify-beetwen items-center ">
                 <p class="font-bold text-[24px] text-blue-500 font-bold text-center">Projects</p>
                 <h1 class="font-bold text-[48px] font-bold text-center">What I have made?</h1>
-                    <div class="flex flex-wrap justify-center items-center gap-10 mt-10">
-                       <div v-for="el in data.data._rawValue" :key="el.id" class="w-[30%] h-full border card grid justify-beetwen shadow-lg shadow-gray-600 ">
-                            <img :src="el.project_img" alt="" class="w-full h-[300px]">
-                            <div class="px-5 py-5 grid gap-3 border-4 border-t-gray-500  ">
+                    <div class="project_s flex flex-wrap justify-center items-center gap-10 mt-10">
+                       <div v-for="el in data.data._rawValue" :key="el.id" class="cards w-[30%] h-full border card grid justify-beetwen shadow-lg shadow-gray-600 ">
+                            <img :src="el.project_img" alt="" class="projectimg w-full h-[300px]">
+                            <div class="projecttext px-5 py-5 grid gap-3 border-4 border-t-gray-500  ">
                                 <h1 class="font-bold text-[24px]">{{el.project_name}}</h1>
                                 <p class="text-[18px] h-[60px]">{{el.description}}</p>
                                 <div class="w-full text-blue-600 flex items-center justify-beetwen gap-6">
@@ -91,8 +91,111 @@
 </script>
 
 <style lang="scss" >
-*{
-    scroll-behavior: smooth;
-}
-
+    *{
+        scroll-behavior: smooth;
+    }
+    @media only screen and (max-width: 1024px) {
+        .mypic{
+            width: 300px;
+            margin-left: 50px;
+        }
+        .projectimg{
+            height: 170px;
+        }
+        .projecttext h1{
+            font-size: 14px;
+        }
+        .projecttext p{
+            font-size: 12px;
+        }
+        .projecttext a{
+            font-size: 14px;
+        }
+    }
+    @media only screen and (max-width: 876px) {
+        .hi h1{
+            font-size: 24px;
+        }
+        .mypic{
+            width: 250px;
+            margin-left: 50px;
+        }
+        .projectimg{
+            height: 170px;
+        }
+        .projecttext h1{
+            font-size: 14px;
+        }
+        .projecttext p{
+            font-size: 12px;
+        }
+        .projecttext a{
+            font-size: 14px;
+        }
+    }
+    @media only screen and (max-width: 668px) {
+        .hi{
+            display: grid;
+            padding-left: -50px;
+         }
+        .hi h1{
+           font-size: 18px;
+        }
+        .hi p{
+            font-size: 14px;
+        }
+        .hi a{
+            display: none;
+        }
+        .myypic{
+            width: 100%;
+        }
+        .mypic{
+            width: 250px;
+            justify-items: center;
+          
+        }
+        .about1{
+            width: 100%;
+            padding: 10px;
+        }
+        .about1 h1{
+            
+            text-align: center;
+        }
+        .text p{
+            font-size: 14px;
+        }
+        .project_s{
+            display: grid;
+        }
+        .cards{
+            width: 100%;
+        }
+        .projectimg{
+            width: 100%;
+            height: 250px;
+        }
+        .projecttext h1{
+            font-size: 20px;
+        }
+        .projecttext p{
+            font-size: 16px;
+        }
+        .projecttext a{
+            font-size: 14px;
+        }
+        .abpic{
+            display: none;
+        }
+        .haq{
+            text-align: center;
+        }
+        .about{
+            width: 100%;
+            text-align: center;
+            padding-left: 10px;
+        }
+       
+    }
 </style>
