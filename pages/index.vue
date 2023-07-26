7b5c5c6efc85e49e9d14f5ddc046f27934db0be8
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-07-26 10:45:06 +0500

    portfolio update

--- pages/index.vue
+++ pages/index.vue
@@ -34,10 +34,10 @@
                         <p class="font-bold text-[24px] text-blue-500 font-bold">About</p>
                     </div>
                     <div>
-                        <h1 class="font-bold text-[48px] font-bold">About me</h1>
+                        <h1 class="font-bold text-[48px] font-bold">About myself</h1>
                     </div>
                     <div class="text">
-                        <p class="text-[20px] text-justify">I am, Usmanov Elyor Elmuratovich, was born in 1987 in a family of workers. I am married, I have 4 children. I graduated from high school in 1994-2002. In 2002-2005, I graduated from Uchkurgan College of Economics, majoring in "Information Technologies". From 2005 to 2022, I had my own "Radio electronics and computer repair" workshop and worked there. In 2022-2023, I studied in the "Foundation" and "Bootcamp Full-stack (NodeJS+VueJs)" courses at the "Najot Ta'lim" educational center.</p>
+                        <p class="text-[20px] text-justify">I am Usmanov Elyor Elmuratovich and was born in 1987 in a family of workers. I am married, I have 4 children. I graduated from high school in 1994-2002. In 2002-2005, I graduated from Uchkurgan College of Economics, majoring in "Information Technologies". From 2005 to 2022, I had my own "Radio electronics and computer repair" workshop and worked there. In 2022-2023, I studied in the "Foundation" and "Bootcamp Full-stack (NodeJS+VueJs)" courses at the "Najot Ta'lim" educational center.</p>
                     </div>
                 </div>
             </div>
